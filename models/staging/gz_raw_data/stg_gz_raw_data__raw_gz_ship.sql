d393aa3d0eedc76cef2e752cbd1d8afe225fa3f0
mourad-5981@hotmail.fr <mourad-5981@hotmail.fr>
2026-04-29 12:38:29 +0000

    renaming columns

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -11,12 +11,10 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
-        ship_cost
+        CAST (ship_cost AS float64) AS ship_cost
 
     from source
-
 )
 
 select * from renamed
